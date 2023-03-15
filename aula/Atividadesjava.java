0c27526f0641bf34e7b1aa5e1be23778cd82dae1
unknown <raylanes315@gmail.com>
2023-03-15 20:48:52 -0300

    Atividade 1

---
+++ aula/Atividadesjava.java
@@ -0,0 +1,16 @@
+package Exercício;
+
+public class Atividadesjava {
+
+	public static void main(String [] args) {
+	
+	float  salário,abono,novosal;
+	
+	System.out.printf("Informe o seu salario: ");
+	System.out.printf("Informe o valor do seu abono: ");
+	
+	novosal = (salário - abono);
+	
+	System.out.printf("Seu novo salário é igual a: "+novosal);
+	}
+}
