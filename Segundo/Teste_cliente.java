c4de539a667f393d97eda7789010bc6c7a5f5816
Raylane <86316691+yarsico@users.noreply.github.com>
2023-03-28 22:57:20 -0300

    Add files via upload

---
+++ Segundo/Teste_cliente.java
@@ -0,0 +1,11 @@
+package POO;
+
+public class Teste_cliente {
+
+	public static void main(String[] args) {
+   
+		Cliente leia = new Cliente("Ray","05/01/2023","5522",22,"13203031612");
+		leia.visualizar();
+	}
+   
+}
